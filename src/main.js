9abcbcc0ef1cf792629cf8e96cab5e3fc9d85b9b
chenmiao123 <951701394@qq.com>
2021-04-13 21:37:31 +0800

    添加axios注释

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ Vue.config.productionTip = false
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import "./assets/index.css"
-
+// 引入axios 模块
 import axios from "axios"
 
 axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1';
